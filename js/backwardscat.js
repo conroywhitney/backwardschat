a11e0a5db4e05a939f4646c75a78a71f8920906b
Conroy Whitney <conroy@Conroys-MBP.local>
2014-05-03 18:25:56 -0400

    clones like 1000000 times

--- js/backwardscat.js
+++ js/backwardscat.js
@@ -5,6 +5,14 @@ scanForChatWindows = function() {
     var content = jQuery(this).val();
     console.log("textarea [" + id + "]");
     console.log(content);
+    console.log("parent");
+    var parent = jQuery(this).parent();
+    console.log(parent);
+    var clone = jQuery(this).clone(true);
+    console.log("clone");
+    console.log(clone);
+    clone.appendTo(parent);
+    console.log("should have a duplicate now)");
   });
 
   setTimeout(scanForChatWindows, 5000);
