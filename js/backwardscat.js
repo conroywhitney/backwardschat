6262af11adbe2a80629b96bfa73503c4b52da1b5
Conroy Whitney <conroy@Conroys-MBP.local>
2014-05-03 18:57:23 -0400

    two windows -- plaintext & encrypted

--- js/backwardscat.js
+++ js/backwardscat.js
@@ -1,22 +1,28 @@
 scanForChatWindows = function() {
   console.log("scanForChatWindows()...");
   jQuery("textarea.ad3").not(".backwardscat").each(function() {
-    var id = jQuery(this).attr("id");
-    var content = jQuery(this).val();
-    var parent = jQuery(this).parent();
-    console.log("textarea [" + id + "]");
-    console.log("content");
-    console.log(content);
+    var $gchat = jQuery(this);
+    var $parent = $gchat.parent();
+    var i_id = $gchat.attr("id");
 
-    if(parent.data("cloned")) {
-      console.log("already cloned! skipping");
+    console.log("textarea [" + i_id + "]");
+
+
+    if($parent.data("cloned")) {
+      var $bcat = $parent.find("textarea.backwardscat");
+
+      var s_encrypted = $gchat.val();
+      var s_plaintext = $bcat.val();
+
+      console.log("plaintext: " + s_plaintext);
+      console.log("encrypted: " + s_encrypted);
     } else {
-      console.log("haven't already cloned.... cloning!");
-      var clone = jQuery(this).clone(true);
-      clone.addClass("backwardscat");
-      clone.appendTo(parent);
-      console.log("should have a duplicate now)");
-      parent.data("cloned", true);
+      console.log("setting up secure chat...");
+      var $clone = $gchat.clone(true);
+      $clone.addClass("backwardscat");
+      $clone.appendTo($parent);
+      $parent.data("cloned", true);
+      console.log("secure chat enabled.");
     }
   });
 
