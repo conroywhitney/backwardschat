8380e911c521ed153c56447c938a736bcfcc7923
Conroy Whitney <conroy@Conroys-MBP.local>
2014-05-03 18:07:33 -0400

    textarea binding -- not great, but at least can find the damn textarea and bind a click (even though it keeps binding multiple clicks damnit)

--- js/backwardscat.js
+++ js/backwardscat.js
@@ -4,12 +4,43 @@ console.log("Backwardscat: loaded with jQuery in GMail context... Proof:");
 console.log(jQuery("#loading"));
 */
 
-// Called when the user clicks on the browser action.
-chrome.browserAction.onClicked.addListener(function(tab) {
-  alert('clicked the icon thingy');
-  console.log("ummmmmmmmmm");
-  console.log(jQuery("#loading"));
-  jQuery("#loading").show();
-  alert('wtf mate?');
+/*
+var insertedNodes = [];
+var observer = new WebKitMutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+        for(var i = 0; i < mutation.addedNodes.length; i++)
+            insertedNodes.push(mutation.addedNodes[i]);
+    })
 });
+observer.observe(document, {
+    childList: true
+});
+console.log(insertedNodes);
+*/
 
+/*
+jQuery("input[type=textarea].ad3").on('click', function() {
+  alert("clicked on one of the chat boxes");
+});
+*/
+
+function checkDOMChange() {
+  jQuery('textarea.ad3').each(function() {
+    ta_id = jQuery(this).attr('id');
+    console.log("Chat ID: " + ta_id);
+    jQuery(this).on("click", function() {
+      alert("clicked on textarea: " + ta_id);
+    });
+  });
+
+  // call the function again after 100 milliseconds
+  setTimeout( checkDOMChange, 5000 );
+}
+
+/*
+  jQuery('textarea.ad3').on("focus", function() {
+    alert("clicked me: " + jQuery(this).attr("id"));
+  });
+*/
+
+checkDOMChange();
