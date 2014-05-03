b10e7dbb2b9478f5173669c4488b1a033b774702
Conroy Whitney <conroy@Conroys-MBP.local>
2014-05-03 18:44:16 -0400

    only clone once, not 10000 times

--- js/backwardscat.js
+++ js/backwardscat.js
@@ -1,18 +1,23 @@
 scanForChatWindows = function() {
   console.log("scanForChatWindows()...");
-  jQuery("textarea.ad3").each(function() {
+  jQuery("textarea.ad3").not(".backwardscat").each(function() {
     var id = jQuery(this).attr("id");
     var content = jQuery(this).val();
-    console.log("textarea [" + id + "]");
-    console.log(content);
-    console.log("parent");
     var parent = jQuery(this).parent();
-    console.log(parent);
-    var clone = jQuery(this).clone(true);
-    console.log("clone");
-    console.log(clone);
-    clone.appendTo(parent);
-    console.log("should have a duplicate now)");
+    console.log("textarea [" + id + "]");
+    console.log("content");
+    console.log(content);
+
+    if(parent.data("cloned")) {
+      console.log("already cloned! skipping");
+    } else {
+      console.log("haven't already cloned.... cloning!");
+      var clone = jQuery(this).clone(true);
+      clone.addClass("backwardscat");
+      clone.appendTo(parent);
+      console.log("should have a duplicate now)");
+      parent.data("cloned", true);
+    }
   });
 
   setTimeout(scanForChatWindows, 5000);
