f66f788f044aeceaa990eadd99e157aa266552f8
Conroy Whitney <conroy@Conroys-MBP.local>
2014-05-03 18:15:28 -0400

    finds text area and pulls out content

--- js/backwardscat.js
+++ js/backwardscat.js
@@ -1,46 +1,13 @@
-/*
-alert("Loaded Backwardscat in GMail context");
-console.log("Backwardscat: loaded with jQuery in GMail context... Proof:");
-console.log(jQuery("#loading"));
-*/
-
-/*
-var insertedNodes = [];
-var observer = new WebKitMutationObserver(function(mutations) {
-    mutations.forEach(function(mutation) {
-        for(var i = 0; i < mutation.addedNodes.length; i++)
-            insertedNodes.push(mutation.addedNodes[i]);
-    })
-});
-observer.observe(document, {
-    childList: true
-});
-console.log(insertedNodes);
-*/
-
-/*
-jQuery("input[type=textarea].ad3").on('click', function() {
-  alert("clicked on one of the chat boxes");
-});
-*/
-
-function checkDOMChange() {
-  jQuery('textarea.ad3').each(function() {
-    ta_id = jQuery(this).attr('id');
-    console.log("Chat ID: " + ta_id);
-    jQuery(this).on("click", function() {
-      alert("clicked on textarea: " + ta_id);
-    });
+scanForChatWindows = function() {
+  console.log("scanForChatWindows()...");
+  jQuery("textarea.ad3").each(function() {
+    var id = jQuery(this).attr("id");
+    var content = jQuery(this).val();
+    console.log("textarea [" + id + "]");
+    console.log(content);
   });
 
-  // call the function again after 100 milliseconds
-  setTimeout( checkDOMChange, 5000 );
+  setTimeout(scanForChatWindows, 5000);
 }
 
-/*
-  jQuery('textarea.ad3').on("focus", function() {
-    alert("clicked me: " + jQuery(this).attr("id"));
-  });
-*/
-
-checkDOMChange();
+scanForChatWindows();
